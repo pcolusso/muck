ef61b355f98c30d9f8da146ba5be1d9b25be4bf7
Paul Colusso <paulcolusso@gmail.com>
2025-06-18 19:43:52 +1000

    Revert "use worktrees?"
    This reverts commit cb315dc7d5880c8c05c10c2232b147139fcc9afc.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use anyhow::{Context, Result, bail};
 use clap::{Parser, Subcommand};
-use git2::{BranchType, Repository};
-use std::{env, fs, process};
+use git2::Repository;
+use std::{process::Command, thread::sleep, time::Duration};
 
 #[derive(Parser, Debug)]
 #[command(author)]
 struct App {
     #[arg(short, long, default_value = "master")]
     main: String,
+    #[arg(short, long, default_value = "scratch")]
+    scratch: String,
     #[command(subcommand)]
     command: Commands,
     #[arg(short, long, default_value_t = true)]
@@ -23,98 +25,67 @@ enum Commands {
 fn main() -> Result<()> {
     let app = App::parse();
     let repo = Repository::discover(".").context("Not a git repository")?;
-    let current_branch = repo
-        .head()
-        .context("Repo may have no commits?")?
-        .shorthand()
-        .unwrap_or("HEAD")
-        .to_string();
 
     match &app.command {
         Commands::Checkout => {
-            // Confirm we're not in a worktree.
-            if repo
-                .worktrees()?
-                .iter()
-                .any(|wt| matches!(wt, Some(s) if *s == current_branch))
-            {
-                bail!("We're in a worktree!");
+            let current = repo.head().context("Repo may have no commits?")?.shorthand().unwrap_or("HEAD").to_string();
+
+            if current != app.main {
+                bail!("Not on the main branch");
             }
 
-            let name = format!(
-                "scratch-{}",
-                petname::petname(2, "-").expect("Wuh? No random?")
-            );
-            let dest = env::temp_dir().join(&name);
+            // Reset the branch
+            if let Ok(mut branch) = repo.find_branch(&app.scratch, git2::BranchType::Local) {
+                println!("Deleting existing scratch branch...");
+                sleep(Duration::from_secs(3));
+                branch
+                    .delete()
+                    .context("Failed to delete existing scratch branch")?;
+                println!("Deleted existing branch '{}'", &app.scratch);
+            }
 
-            repo.worktree(&name, &dest, None)?;
+            let obj = repo.revparse_single("HEAD")?;
+            repo.branch(&app.scratch, &obj.peel_to_commit()?, false)
+                .context("Failed to create scratch branch")?;
 
-            let shell = env::var("SHELL").unwrap_or("/bin/bash".into());
-            // We can't affect the shell that spawned us, but we can spawn a subshell!
-            process::Command::new(&shell).current_dir(dest).status()?;
+            repo.set_head(&format!("refs/heads/{}", &app.scratch))
+                .context("Failed to switch to scratch branch")?;
 
-            // In theory, we could auto squash merge back in...
             println!(
-                "You're back in the real world, the worktree you were in is {}, from {}",
-                name, current_branch
+                "Created and switched to branch '{}' from '{}'",
+                &app.scratch, current
             );
+            println!("Go nuts!")
         }
         Commands::Checkin => {
-            let worktrees = repo.worktrees()?;
-            // Confirm we in a worktree.
-            if worktrees
-                .iter()
-                .any(|wt| matches!(wt, Some(s) if *s == current_branch))
-            {
-                bail!("We're in a worktree!");
+            let current = repo.head()?.shorthand().unwrap_or("HEAD").to_string();
+            if current != app.scratch {
+                bail!("Not on the scratch branch");
             }
 
-            let mut most_recent = None;
-            // Find the most recently updated worktree
-            for w in worktrees.iter().flatten() {
-                let worktree = repo.find_worktree(w)?;
-                let path = worktree.path();
-                if let Ok(metadata) = fs::metadata(path) {
-                    if let Ok(modified_time) = metadata.modified() {
-                        match &most_recent {
-                            None => {
-                                most_recent = Some((worktree, modified_time));
-                            }
-                            Some((_, current_time)) => {
-                                if modified_time > *current_time {
-                                    most_recent = Some((worktree, modified_time));
-                                }
-                            }
-                        }
-                    }
-                }
+            let root_ref = format!("refs/heads/{}", &app.main);
+            repo.set_head(&root_ref)
+                .context("Failed to set HEAD to root branch")?;
+            println!("Checked out '{}'", &app.main);
+
+            let status = Command::new("git")
+                .arg("merge")
+                .arg("--squash")
+                .arg(&app.scratch)
+                .status()
+                .context("Failed to start git squash merge")?;
+
+            if !status.success() {
+                return Err(anyhow::anyhow!("git merge --squash failed"));
             }
-
-            let worktree = most_recent.expect("Couldn't find a worktree.").0;
-            let dest = worktree.path();
-            let w_repo = Repository::open(dest)?;
-            let w_head = w_repo.head()?;
-            let w_branch = w_head.shorthand().expect("Detached?");
-
-            // Keep it simple, we're just gonna use the command for now
-
-            process::Command::new("git")
-                .args(["merge", "--squash", w_branch])
-                .status()
-                .context("Failed to squash merge")?;
-
-            process::Command::new("git")
-                .args(["worktree", "remove", w_branch])
-                .status()
-                .context("Faliled to remove")?;
 
             if app.use_smerge {
-                process::Command::new("smerge")
+                Command::new("smerge")
                     .arg(".")
                     .status()
                     .context("Unable to open sublime")?;
             } else {
-                process::Command::new("git")
+                Command::new("git")
                     .arg("commit")
                     .status()
                     .context("Failed to invoke git commit")?;
